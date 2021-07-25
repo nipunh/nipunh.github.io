05ad37f7dc823f853bc33db975d6de0b21e03a8a
Nipun Hedaoo <nipunhedaoo@gmail.com>
2021-07-25 19:51:08 +0530

    completed

--- js/main.js
+++ js/main.js
@@ -33,10 +33,10 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 
-// =================================== QUalification tabs ======================
+// =================================== Qualification tabs ======================
 
-const tabs = document.querySelectorAll('[data-target]'),
-tabContents = document.querySelectorAll('[data-content')
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
@@ -44,7 +44,7 @@ tabs.forEach(tab => {
     const target = document.querySelector(tab.dataset.target)
 
     tabContents.forEach(tabContent => {
-      tabContent.classList.remove('qulification__active')
+      tabContent.classList.remove('qualification__active')
     })
 
     target.classList.add('qualification__active');
@@ -57,3 +57,48 @@ tabs.forEach(tab => {
 
   })
 })
+
+// =================================== Portfolio ======================
+
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop : true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable : true
+  },
+});
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)

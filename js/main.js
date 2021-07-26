ec2f346b8948b629bd0f1095881ad4d400a4342e
Nipun Hedaoo <nipunhedaoo@gmail.com>
2021-07-26 20:07:18 +0530

    skills updated

--- js/main.js
+++ js/main.js
@@ -74,6 +74,30 @@ let swiper = new Swiper(".portfolio__container", {
 });
 
 
+// ================================== Skills ==========================
+
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className;
+
+  for(i = 0; i< skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close';
+  }
+
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+
+}
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener('click', toggleSkills);
+})
+
+
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
